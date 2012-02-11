59dbf4054244a70d9f219f806fdf6d8716ef2f23
Eli Mitchell <dev@cyberstream.us>
2012-02-11 09:26:29 -0900

    scrolling now works in the comment frame

--- includes/include.js
+++ includes/include.js
@@ -1,15 +1,39 @@
 opera.extension.onmessage = function(event) {    
     resizeFrame = function(e) {                    
-                                if (window.innerHeight - e.clientY >= 10 && e.clientY > 10)
-                                    document.getElementById('fix-the-web-comment-frame').style.height = 100 - Math.floor((e.clientY / window.innerHeight) * 100) + '%'
+                                if (e.clientY > 0 && window.innerHeight - e.clientY >= 10 && e.clientY > 30) {
+                                    var ftw_frame = document.getElementById('fix-the-web-comment-frame'),
+                                          ftw_resize_bar = document.getElementById('resize-frame'),
+                                          ftw_resize_bar_height = parseInt(ftw_resize_bar.style.height) || 15
+                                    
+                                    // set the frame height to wherever the cursor is
+                                    ftw_frame.style.height = 100 - Math.floor((e.clientY / window.innerHeight) * 100) + '%'
+                                    
+                                    var bar_height_percentage = ((ftw_resize_bar_height / window.innerHeight).toFixed(3)) * 100 // round to the nearest tenth place
+                                    ftw_resize_bar.style.bottom = (parseInt(ftw_frame.style.height) + bar_height_percentage) + '%'; // place the resize bar right above the comment frame
+                                }
                             }
+                            
+    resetFrameBar = function() {
+                                    var ftw_frame = document.getElementById('fix-the-web-comment-frame'),
+                                          resize_frame = document.getElementById('resize-frame');
+                                          
+                                          if (ftw_frame && resize_frame) {                                              
+                                            ftw_resize_bar_height = parseInt(resize_frame.style.height) || 15
+        
+                                            var resize_frame_height = parseInt(resize_frame.style.height) || 15
+                                            bar_height_percentage = ((resize_frame_height / window.innerHeight).toFixed(3)) * 100 // round to the nearest tenth place
+                                            resize_frame.style.bottom = (40 + bar_height_percentage) + '%'; // place the resize bar right above the comment frame
+                                        }
+                                }
     
     if (event.data == 'reply') {
         window.addEventListener('load', function () {
             event.source.postMessage('initialize badge')
         }, false)
     } else if (event.data.frame_content) {
-        document.getElementById('frame-content').innerHTML = event.data.frame_content;
+        document.getElementById('fix-the-web-comment-frame').innerHTML = event.data.frame_content;
+        
+        window.addEventListener('resize', resetFrameBar, false); // readjust the positioning of the resize bar whenever the window is resized
     } else if (event.data == "load comments frame") {
         if (!document.getElementById('fix-the-web-comment-frame')) {
             var frame_element = document.createElement('div');
@@ -17,13 +41,10 @@ opera.extension.onmessage = function(event) {
                   frame_element.className = 'fadeout_state';
             
             var resize_frame = document.createElement('div'),
-                  close_frame = document.createElement('span')
-                  frame_content_container = document.createElement('div');
+                  close_frame = document.createElement('span');
                   
             resize_frame.id = 'resize-frame';
             close_frame.id = 'close-frame';
-            frame_content_container.id = 'frame-content';
-            frame_content_container.appendChild(document.createTextNode('Loading...'))
             
             close_frame.appendChild(document.createTextNode('x'));            
             resize_frame.appendChild(close_frame);
@@ -31,13 +52,14 @@ opera.extension.onmessage = function(event) {
             // close the frame when the "x" is clicked
             close_frame.addEventListener('click', function() { 
                 document.getElementById('fix-the-web-comment-frame').className = 'fadeout_state' // trigger frame fade-out animation with CSS3 transitions by changing the className
+                document.getElementById('resize-frame').className = 'hidden'
 
                 setTimeout(function() {
                     document.getElementById('fix-the-web-comment-frame').parentNode.removeChild(document.getElementById('fix-the-web-comment-frame'));
+                    document.getElementById('resize-frame').parentNode.removeChild(document.getElementById('resize-frame'))
+                    window.removeEventListener('resize', resetFrameBar, false);
                 }, 1000)
             }, false);
-            
-            frame_element.appendChild(resize_frame); // append the resize/close frame bar to the main frame
             
             // trigger the drag function whenever the mouse button is pressed and the mouse is moved            
             resize_frame.addEventListener('mousedown', function () {
@@ -45,46 +67,47 @@ opera.extension.onmessage = function(event) {
             }, false);
             
             // detach the drag function when the mouse button is released
-
-            frame_element.addEventListener('mouseup', function() {
+            window.addEventListener('mouseup', function() {
                 window.removeEventListener('mousemove', resizeFrame, false);
-                
-                // deselect text that was selected while resizing the panel                
-                var sel = window.getSelection();
-                sel.collapseToStart();
-                sel.collapseToEnd();
             }, false);
             
             // create a styles element for the frame's styles            
             var frame_style_element = document.createElement('style'),
-                  frame_styles = '::selection {background: transparent !important;}';
+                  frame_styles = ''; //::selection {background: transparent !important;}'; // make selection invisible
 
-            frame_styles += '#fix-the-web-comment-frame {position: fixed; z-index:123456789 !important; box-shadow:0 0 90px #eee; background: -o-linear-gradient(bottom, rgba(255,255,255,1) 30%, rgba(255,255,255,.85)); left: 0; margin: 0; padding-bottom: 5px; -o-transition:bottom 0.5s ease-in-out, opacity 0.5s ease-in-out, width 500ms ease-in-out, height 100ms ease-in-out;}'
-            frame_styles += '.normal_state{height:40%; width:100%; opacity:1; bottom:0px} .fadeout_state {opacity:0; bottom:-20px;} .minimized_state #resize-frame {cursor:pointer}' 
-            frame_styles += 'div#fix-the-web-comment-frame.minimized_state {bottom:0; height:15px; width:40px; padding:0;} .minimized_state #resize-frame:before {background:none}'
-            frame_styles += '#resize-frame {width: 100%; position: relative; float: left; left: 0; top: 0; height: 15px; background-image:-o-linear-gradient(bottom, #777, #aaa); cursor:n-resize} #resize-frame:active {background-image:-o-linear-gradient(top, #333, #555)}'
-            frame_styles += '#resize-frame:before { position:absolute; left:46%; content:""; width:100px; height:100%; background:-o-radial-gradient(25% 50%, 3px 3px, #555 100%, transparent),-o-radial-gradient(36% 50%, 3px 3px, #555 100%, transparent), -o-radial-gradient(47% 50%, 3px 3px, #555 100%, transparent), -o-radial-gradient(25% 54%, 3px 3px, #bbb 100%, transparent),-o-radial-gradient(36% 54%, 3px 3px, #bbb 100%, transparent), -o-radial-gradient(47% 54%, 3px 3px, #bbb 100%, transparent); }'
+            frame_styles += '#fix-the-web-comment-frame {position: fixed; z-index:123456788 !important; box-shadow:0 0 90px #eee; font-family: "myriad pro", "arial", "lucida grande", "lucida sans unicode", "bitstream vera sans", "dejavu sans", "trebuchet ms", sans-serif; font-size: 16px; padding: 10px 1.5% 5px 1.5%; font-weight:bold; color:#333; background: -o-linear-gradient(bottom, rgba(255,255,255,1) 30%, rgba(255,255,255,.85)); left: 0;  -o-transition:bottom 0.5s ease-in-out, opacity 0.5s ease-in-out, width 0.5s ease-in-out, height 0.1s ease-in-out;}'
+            frame_styles += '.normal_state{height:40%; width:97%; opacity:1; bottom:0px} .fadeout_state {opacity:0; bottom:-20px;}' 
+            frame_styles += '#resize-frame {-o-transition:opacity 0.5s ease-in-out; z-index:123456789 !important; width: 100%; position: fixed; left: 0; height: 15px; background-image:-o-linear-gradient(bottom, #777, #aaa); cursor:n-resize} #resize-frame:active {background-image:-o-linear-gradient(top, #333, #555); box-shadow:0 0 5px #888}'
+            frame_styles += '#resize-frame.hidden {opacity:0} #resize-frame.display {opacity:1} #resize-frame:before { position:absolute; left:46%; content:""; width:100px; height:100%; background:-o-radial-gradient(25% 50%, 3px 3px, #555 100%, transparent),-o-radial-gradient(36% 50%, 3px 3px, #555 100%, transparent), -o-radial-gradient(47% 50%, 3px 3px, #555 100%, transparent), -o-radial-gradient(25% 54%, 3px 3px, #bbb 100%, transparent),-o-radial-gradient(36% 54%, 3px 3px, #bbb 100%, transparent), -o-radial-gradient(47% 54%, 3px 3px, #bbb 100%, transparent); }'
             frame_styles += '#close-frame {cursor:pointer; font:bold 16px sans-serif; color: #eee; float:right; top:-5px; margin: 2px 0px; padding: 0 4px; position:relative; border-left:1px solid transparent;} #close-frame:hover {background:rgba(255,255,255,.75); color:#444; border-left: 1px solid #fff}';
-            frame_styles += '#fix-the-web-comment-frame #frame-content {font-family: "myriad pro", "arial", "lucida grande", "lucida sans unicode", "bitstream vera sans", "dejavu sans", "trebuchet ms", sans-serif; font-size: 16px; padding:20px 10px 5px 10px; font-weight:bold; color:#333;}';
             
             frame_style_element.setAttribute('type', 'text/css');
             frame_style_element.appendChild(document.createTextNode(frame_styles));
             
-            frame_element.appendChild(frame_content_container); // append the main container so the scrolling takes place in it 
+            // place the resize bar where it should go                
+            var resize_frame_height = parseInt(resize_frame.style.height) || 15
+                  bar_height_percentage = ((resize_frame_height / window.innerHeight).toFixed(3)) * 100 // round to the nearest tenth place
+                  resize_frame.style.bottom = (40 + bar_height_percentage) + '%'; // place the resize bar right above the comment frame
             
-            // insert the frame element and the style element into the HTML page            
+            // insert the resize bar, the frame element and the style element into the HTML page             
             document.body.appendChild(frame_element);
+            document.body.appendChild(resize_frame);
             document.head.appendChild(frame_style_element);
             
             frame_element.className = 'fadeout_state' 
+            resize_frame.className = 'hidden'
+            
             setTimeout(function() {
                 frame_element.className = 'normal_state' // trigger frame fade-in animation with CSS3 transitions by changing the className
+                resize_frame.className = 'display'
             }, 100)
             
             event.source.postMessage("get_frame_content");
         }
     }
 }    
+
+// get the patches.js script from localStorage and create a script element on the page with its contents
 
 window.addEventListener('DOMContentLoaded', function() {    
     if (typeof widget.preferences.patches_js == 'undefined') widget.preferences.patches_js = '';
