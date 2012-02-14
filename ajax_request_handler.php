2afbbe9d089fbabf6994288fe94c0758855340e1
greench <greench@myopera.com>
2012-02-14 18:57:36 +0200

    initial home page

--- ajax_request_handler.php
+++ ajax_request_handler.php
@@ -1,51 +1,12 @@
 <?php 
 header("Cache-Control: no-cache, must-revalidate");
 require_once 'db.php'; // include the database configuration file
-
+error_reporting(E_ALL);
 if (isset($_GET) && count($_GET)) {
-    if ($_GET['mode'] == 'get_OS') {
-        // detects the OS by reading the HTTP_USER_AGENT string and extracting details about the OS from it. 
-
-        $OSList = array (
-                // Match user agent string with operating systems
-                'Windows 3.11' => 'Win16',
-                'Windows 95' => '(Windows 95)|(Win95)|(Windows_95)',
-                'Windows 98' => '(Windows 98)|(Win98)',
-                'Windows 2000' => '(Windows NT 5.0)|(Windows 2000)',
-                'Windows XP' => '(Windows NT 5.1)|(Windows XP)',
-                'Windows Server 2003' => '(Windows NT 5.2)',
-                'Windows Vista' => '(Windows NT 6.0)',
-                'Windows 7' => '(Windows NT 7.0)',
-                'Windows NT 4.0' => '(Windows NT 4.0)|(WinNT4.0)|(WinNT)|(Windows NT)',
-                'Windows ME' => 'Windows ME',
-                'Open BSD' => 'OpenBSD',
-                'Sun OS' => 'SunOS',
-                'Linux' => '(Linux)|(X11)',
-                'Mac OS' => '(Mac_PowerPC)|(Macintosh)',
-                'QNX' => 'QNX',
-                'BeOS' => 'BeOS',
-                'OS/2' => 'OS/2',
-                'Search Bot'=>'(nuhk)|(Googlebot)|(Yammybot)|(Openbot)|(Slurp)|(MSNBot)|(Ask Jeeves/Teoma)|(ia_archiver)'
-        );
-
-        $OS = '';
-        
-        // Loop through the array of user agents and matching operating systems
-        foreach($OSList as $current_OS => $match) {
-            // Find a match
-            if (preg_match ('~' . $match . '~i', $_SERVER['HTTP_USER_AGENT'])) {
-                // exit the loop because we found the correct match
-
-                $OS = $current_OS;
-                break;
-            }
-        }
-
-        exit ($OS); // echo this for the AJAX request to get
-    } elseif ($_GET['mode'] == 'submit error') { 
+    if ($_GET['mode'] == 'submit error') { 
         // validate the form fields
-        if (!isset($_GET['category']) || !preg_match('/(1|2|3)/', $_GET['category'])) 
-            exit ('Please select a valid category for the error report.' . $_GET['category']);
+        if (!isset($_GET['category']) || !($_GET['category'] != 1 && $_GET['category'] != 2 && $_GET['category'] != 3)) 
+            exit ('Please select a valid category for the error report.');
         elseif (!isset($_GET['description']) || strlen($_GET['description']) < 5) 
             exit ('Please fill out the description field with a description of the problem you encountered.');
         elseif (!isset($_GET['system']) || !isset($_GET['version']) || !isset($_GET['build']))
@@ -74,18 +35,16 @@ if (isset($_GET) && count($_GET)) {
         $stmt = $db->stmt_init();
         
         if ($_GET['method'] == 'domain' && isset($_GET['domain'])) {
-            $fetch_mode = 'website.';
             $query = "SELECT 
             username, language, category, report, page, opera_version, opera_build, operating_system, additional_information, DATE_FORMAT(time, '%M %e, %Y at %l:%i%p')
             FROM reports WHERE post_type = 0 AND domain = ?";
             $bind_variable = $_GET['domain'];
         } elseif ($_GET['method'] == 'page' && isset($_GET['page'])) {
-            $fetch_mode = 'page.';
             $query = "SELECT 
             username, language, category, report, page, opera_version, opera_build, operating_system, additional_information, DATE_FORMAT(time, '%M %e, %Y at %l:%i%p')
             FROM reports WHERE post_type = 0 AND page = ?";
             $bind_variable = $_GET['page'];
-        } else exit ('There was an error fetching the bug reports.');
+        } else exit ('There was an error fetching the bug reports for this website.');
 
         if ($stmt->prepare($query)) {
             $stmt->bind_param('s', $bind_variable);
@@ -97,20 +56,20 @@ if (isset($_GET) && count($_GET)) {
                 $return = '';
                 
                 while ($stmt->fetch()) { // TODO display the comments nicer
-                    $misc = rawurlencode($misc);
-                    
                     $return .= <<<_HERE
-                    <div class="thread">
-                        <span class="username">$username</span> said on $date_time:
-                        <div class="report">"$report"</div>                    
+                    <p>
+                        <em>$username</em> said on $date_time:
+                        "$report"
+                            
                         <div>Page: <em>$page</em></div>
-                        <div>Additional information &mdash; version: $version, build number: $build, operating system: $OS <a href="data:text/plain;charset=utf-8,$misc" target="_blank">plugins and screen information</a></div>
+                        <div>Additional information &mdash; version: $version, build number: $build, operating system: $OS</div>
                     </p>
+                    <hr />
 _HERE;
                 }
                 
                 exit ($return);
-            } else exit ('There were no bugs reported on this ' . $fetch_mode);       
+            } else exit ('There were no bugs reported on this website.');       
         }
     } elseif ($_GET['mode'] == 'get_reports_count' && isset($_GET['method']) && (isset($_GET['page']) || isset($_GET['domain']))) {
         if ($_GET['method'] == 'domain' || $_GET['method'] == 'page') {
@@ -138,5 +97,157 @@ _HERE;
         }
         
         exit ('0');
+    }elseif(($_GET['mode'] == 'get_report_list')){
+        // TODO control $_GET variable health
+        
+        $stmt = $db->stmt_init();
+        
+        // TODO check DATE_FORMAT when multilangualizing
+        $query = "SELECT 
+        id,username, language, category, report, page, domain, opera_version, opera_build, operating_system, additional_information, DATE_FORMAT(time, '%M %e, %Y at %l:%i%p')
+        FROM reports WHERE post_type = 0";
+
+        if(isset($_GET['domain'])){
+            $query.=" AND domain = ?";
+            $bind_domain = $_GET['domain'];
+        }
+
+        if(isset($_GET['order'])){
+            switch ($_GET['order']) {
+                case 'most_followed':
+                    
+                break;
+                case 'popularity':
+                    
+                break;
+                case 'time_asc':
+                    $query.=" ORDER BY time ASC";
+                    break;
+                
+                case 'time_desc':
+                default:
+                    $query.=" ORDER BY time DESC";
+                    break;
+            }
+        }else{
+            $query.=" ORDER BY time DESC";
+        }
+        $query.=" LIMIT ?,?";
+
+
+        // What page that you want to look at
+        if(isset($_GET['page']) && settype($_GET['page'],"int")){
+            $bind_page_number=--$_GET['page'];
+        }
+        else{
+            $bind_page_number="0";
+        }
+
+
+        if(isset($_GET['count']) && settype($_GET['count'],"int")){
+            $bind_count=$_GET['count'];
+        }
+        else{
+            $bind_count="5"; // TODO: Default limit
+        }
+
+        if ($stmt->prepare($query)) {
+
+            if(isset($bind_domain))
+                $stmt->bind_param('sii', $bind_domain,$bind_page_number,$bind_count);
+            else
+            if(isset($bind_page_number) && isset($bind_count))
+                $stmt->bind_param('ii', $bind_page_number,$bind_count);
+
+            $q = $stmt->execute();
+            $stmt->store_result();
+        
+            if ($q && $stmt->num_rows) {
+                $stmt->bind_result($id,$username, $language, $category, $report, $page, $domain_db, $version, $build, $OS, $misc, $date_time);
+
+                $JSON = array();
+                while ($stmt->fetch()) { // TODO display the comments nicer
+                    array_push(&$JSON,
+                        array(  "id"=>$id,
+                                "username"=>$username,
+                                "language"=>$language,
+                                "category"=>$category,
+                                "report"=>$report,
+                                "page"=>$page,
+                                "domain"=>$domain_db,
+                                "date_time"=>$date_time,
+                                "Opera" => $version,
+                                "build" => $build,
+                                "OS" => $OS,
+                                "misc"=>$misc
+                        ));
+                }
+                
+                echo json_encode($JSON);
+                    
+            }
+        }
+    }elseif(($_GET['mode'] == 'get_comment_list')){
+         // TODO control $_GET variable health
+        
+        $stmt = $db->stmt_init();
+        
+        // TODO check DATE_FORMAT when multilangualizing
+        $query = "SELECT 
+        id,username, language, category, report, page, domain, opera_version, opera_build, operating_system, additional_information, DATE_FORMAT(time, '%M %e, %Y at %l:%i%p')
+        FROM reports WHERE 1=1 ";
+
+        if(isset($_GET['domain'])){
+            $query.=" AND domain = ?";
+            $bind_domain = $_GET['domain'];
+        }
+        if(isset($_GET['id']) && settype($_GET['id'], "int")){
+
+            $query.=" AND (id = ? OR report_id = ?)";
+            $bind_report_id=$_GET["id"];
+        }
+        
+        if ($stmt->prepare($query)) {
+
+            if(isset($bind_domain)){
+                if(isset($bind_report_id))
+                    $stmt->bind_param('sii', $bind_domain,$bind_report_id,$bind_report_id);
+                else
+                    $stmt->bind_param('s', $bind_domain);
+            }
+            else{
+                if(isset($bind_report_id))
+                    $stmt->bind_param('ii',$bind_report_id,$bind_report_id);
+            }
+                
+
+            $q = $stmt->execute();
+            $stmt->store_result();
+        
+            if ($q && $stmt->num_rows) {
+                $stmt->bind_result($id,$username, $language, $category, $report, $page, $domain_db, $version, $build, $OS, $misc, $date_time);
+
+                $JSON = array();
+                while ($stmt->fetch()) { // TODO display the comments nicer
+                    array_push(&$JSON,
+                         array(  "id"=>$id,
+                                "username"=>$username,
+                                "language"=>$language,
+                                "category"=>$category,
+                                "report"=>$report,
+                                "page"=>$page,
+                                "domain"=>$domain_db,
+                                "date_time"=>$date_time,
+                                "Opera" => $version,
+                                "build" => $build,
+                                "OS" => $OS,
+                                "misc"=>$misc
+                        ));
+                }
+                
+                echo json_encode($JSON);
+                    
+            }
+        }
     }
 } else echo 'Page not found!';
---
+++ index.php
@@ -0,0 +1,245 @@
+<DOCTYPE! html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<title>Fix the Web Front Panel</title> <!-- // TODO: Change title with suitable one-->
+
+	<style type="text/css">
+    body{
+        font-size:1em;
+        padding:0;
+        margin: 0;
+        
+        font-family: "arial", "lucida grande", "lucida sans unicode", "bitstream vera sans", "dejavu sans", "trebuchet ms", sans-serif;
+    }
+	header{
+        border-bottom:1px solid #dfdfdf;
+        width:100%;
+        padding:0.33em 1em;
+        box-sizing:border-box;
+        position: relative;
+
+    }
+
+    header h1{
+
+        color:yellowgreen;
+        float:left;
+        text-align:center;
+        text-shadow:0 0 2px #000;
+        min-width:100px;
+        width:20%;
+
+    }
+
+    header div {
+        border-left:1px solid #dfdfdf;
+        
+        float:left;
+        padding:0.33em 1em;
+        box-sizing:border-box;
+        width:80%;
+        position: absolute;
+        top:0px;
+        bottom:0px;
+        right:0px;
+    }
+
+	footer{}
+
+    section{
+        width:75%;
+        padding:0.33em 1em;
+        box-sizing:border-box;
+        float:left;
+    }
+
+	article{
+
+        padding:0.33em;
+        position:relative;
+        margin:0.33em;
+    }
+    
+    h6{
+        padding:0;
+        border-bottom:1px solid #999;
+        width:100%;
+        position: relative;
+        margin:0;
+        font-size:1em;
+    }
+    .go-button{
+        position:absolute;
+        top:3em;
+        right:1em;
+        padding:0.33em 1em;
+        border:1px solid #555;
+        box-shadow:0px 0px 2px #000;
+        border-radius:5px;
+        background-color:yellowgreen;
+        color:#fff;
+        font-weight:bold;
+        
+    }
+    .additional-information{
+        position: absolute;
+        bottom:0;
+        right:0;
+        padding:0.11em;
+        font-size:0.8em;
+    }
+    .report-text{
+
+    }
+	aside{
+        width:25%;   
+        padding:0.33em 1em;
+        box-sizing:border-box;
+        float:left;
+    }
+    form label{
+        float:right;
+        clear: both;
+        position: relative;
+    }
+    input[type=number]{
+        width:50px;
+       
+
+    }
+	</style>
+</head>
+<body>
+    <header>
+    <h1>Fix the Web</h1>
+    <div>
+        <h2>
+            What is Fix the Web?
+        </h2>
+        <p>Fix the web is bla bla bla.</p>
+        </header>
+    </div>
+    <section>
+
+    </section>
+        <aside>
+            
+            <h2>Listing Options</h2>
+        
+            <form action="" id="form">
+
+                <label for="domain">Domain
+                    <input type="text" name="domain" id="domain" placeholder="Enter a domain name" <?php if(isset($_GET['domain'])) echo "value=".$_GET['domain'];?>>
+                </label>
+                <label for="page">Page
+                    <input type="number" name="page" id="page" <?php if(isset($_GET["page"])) echo "value=".$_GET["page"];?>>
+                </label>
+                <label for="count">Report count
+                    <input type="number" name="count" id="count" <?php if(isset($_GET["count"])) echo "value=".$_GET["count"];?>>
+                </label>
+                <button type="submit">Submit</button>
+            
+            </form>
+            <div>
+
+            </div>
+        </aside>
+        <script type="text/javascript">
+var HOST="http://localhost/Fix-the-Web-Server-Side/"; // TODO edit this for your system
+
+function reportTemplate(id,username,date_time,report,operaVersion,operaBuildNumber,OS,domain,page){
+    var content='';
+    content="<article><h6><em><a href=''>"+username+"</a></em> said on "+date_time+":</h6><button data-id="+id+" class='go-button'> &gt; </button><p> \
+" +report+"</p><em>"+page+" on "+domain+"</em><span class='additional-information'>"+operaVersion+"."+operaBuildNumber+" on "+OS+"</span></article>";
+    return content;
+}
+
+// If xmlHTTPRequest is succesfull, then write the result into a suitable area
+function resultWriter(data){
+    if(!data) return false;
+    var result=JSON.parse(data);
+    var resultArea='';
+    for (i in result)
+    {
+        a=result[i];
+        resultArea+=reportTemplate(a.id,a.username,a.date_time,a.report,a.Opera,a.build,a.OS,a.domain,a.page);
+    }
+    document.getElementsByTagName("section")[0].innerHTML=resultArea;
+
+    var buttons = document.querySelectorAll(".go-button");
+
+    for (c=0;c<buttons.length;c++){
+        
+        buttons[c].addEventListener("click",function(event){
+            sendRequest("GET",HOST+"ajax_request_handler.php?mode=get_comment_list&id="+event.target.dataset.id,resultWriter,null);
+            //history.pushState({foo: 'bar2'}, 'Comments', "ajax_request_handler.php?mode=get_comment_list&id="+event.target.dataset.id);
+
+        },false);
+    }
+}
+
+
+window.addEventListener("DOMContentLoaded",function(){
+    // index page operations
+    sendRequest("GET",HOST+"ajax_request_handler.php?mode=get_report_list",resultWriter,null);
+
+    document.getElementById("form").addEventListener("submit",function(){
+        // TODO Prevent default action
+        event.preventDefault();
+        event.stopPropagation();
+        var query='&';
+        if(document.getElementById("domain").value)
+            query+="domain="+document.getElementById("domain").value+"&";
+        if(document.getElementById("page").value)
+            query+="page="+document.getElementById("page").value+"&";
+        if(document.getElementById("count").value)
+            query+="count="+document.getElementById("count").value+"&";
+        query+="a=1";
+        
+        sendRequest("GET",HOST+"ajax_request_handler.php?mode=get_report_list"+query,resultWriter,null);
+       // history.pushState({foo: 'bar'}, 'Search Results', "ajax_request_handler.php?mode=get_report_list"+query);
+        return false;
+    },false);
+
+    
+
+},false);
+
+            
+function sendRequest (method, url, callback, params) {
+    var xhr = new XMLHttpRequest();
+   
+    xhr.onreadystatechange = function() {
+        if (this.status == 200 && this.readyState == 4) {
+            if (typeof callback == 'function') callback(this.responseText);
+        }
+    }
+         
+    // serialize the parameters passed into this function, if there are any. 
+    // For example, change {key: 'val', key2: 'val2'} to 'key=val&key2=val2'
+    if (typeof params == 'object' && params) {
+        var serialized_data = '';
+        
+        for (i in params) {
+            if (typeof first_iteration == 'undefined') {
+                serialized_data += i + '=' + encodeURIComponent(params[i]);
+                var first_iteration = true;
+            } else // we need to add an ampersand (&) at the beginning if there are already parameters in the query string
+                serialized_data += '&' + i + '=' + encodeURIComponent(params[i]); 
+        }
+    } else serialized_data = false;    
+    try {
+        if (method.toLowerCase() != 'get') throw 'Invalid method "' + method + '" was specified. AJAX request could not be completed.' ;
+        else {
+            xhr.open(method, url + (serialized_data && serialized_data.length ? '?' + serialized_data : ''), true)
+            xhr.send(null);
+        }
+    } catch(error) { 
+        console.log('Error: ' + error);
+        return false;
+    }
+} // end sendRequest() function
+        </script>
+    </body>
+</html>
