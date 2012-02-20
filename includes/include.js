a60b374c32098a8137bb1ed4948d706b859ca31f
Eli Mitchell <dev@cyberstream.us>
2012-02-19 17:15:58 -0900

    made error report threads collapsible

--- includes/include.js
+++ includes/include.js
@@ -44,7 +44,7 @@ opera.extension.onmessage = function(event) {
                     var page_url = '<a href="' +current.page+ '" title="Page: ' +current.page+ '" target="_blank">' 
                         +(current.page.length > 40 ? current.page.substr(0, 40) + '...' : current.page) + '</a>';
                     
-                    commentFrameHTML += '<p class="thread"><div class="title">Posted by <span class="username">' +current.username+ '</span> on ' +current.date_time+ ' from page ' +page_url+ '</div><div>"' +current.report+ '"</div> Opera version: ' +current.Opera+ ' Opera build: ' +current.build+ ' OS: ' +current.OS+ ' <a href="data:text/plain;charset=utf-8,' +encodeURIComponent(current.misc)+ '" target="_blank">miscellaneous information</a></p><hr />';
+                    commentFrameHTML += '<div class="thread collapsed"><div class="title">Posted by <span class="username">' +current.username+ '</span> on ' +current.date_time+ ' from page ' +page_url+ ' <span class="change_state"></span></div><div class="toggle"><div class="report_body">"' +current.report+ '"</div> Opera version: ' +current.Opera+ ' Opera build: ' +current.build+ ' Platform: ' +current.OS+ ' <a href="data:text/plain;charset=utf-8,' +encodeURIComponent(current.misc)+ '" target="_blank">miscellaneous information</a></div></div>'
                 }
             }
         }
@@ -53,6 +53,22 @@ opera.extension.onmessage = function(event) {
             (language != 'all' ? 'No reports were found. Try viewing reports in all languages.' : 'No reports were found.')
         
         commentFrame.innerHTML = commentFrameHTML; // insert the HTML into the comment frame
+        
+        var change_state_triggers = document.querySelectorAll('#fix-the-web-comment-frame .change_state');
+        
+        for (i = 0; i < change_state_triggers.length; i++) {
+            change_state_triggers[i].addEventListener('click', function() {
+                var thread = this.parentNode.parentNode,
+                      currentClassName = thread.className.match(/(collapsed|expanded)/i)[1],    
+                
+                toggleStates = {
+                    'collapsed' : 'expanded',
+                    'expanded' : 'collapsed'
+                }
+                
+                thread.className = thread.className.replace(/(collapsed|expanded)/i, toggleStates[currentClassName])
+            }, false)
+        }
     } // end populateCommentFrame() function
     
     // process incoming messages and trigger the specified command
@@ -106,10 +122,17 @@ opera.extension.onmessage = function(event) {
                   frame_styles = ''; //::selection {background: transparent !important;}'; // make selection invisible
 
             frame_styles += '#fix-the-web-comment-frame {overflow:auto; position: fixed; z-index:123456788 !important; box-shadow:0 0 90px #eee; font-family: "myriad pro", "arial", "lucida grande", "lucida sans unicode", "bitstream vera sans", "dejavu sans", "trebuchet ms", sans-serif; font-size: 16px; padding: 10px 1.5% 5px 1.5%; font-weight:bold; color:#333; background: -o-linear-gradient(bottom, rgba(255,255,255,1) 30%, rgba(255,255,255,.85)); left: 0;  -o-transition:bottom 0.5s ease-in-out, opacity 0.5s ease-in-out, width 0.5s ease-in-out, height 0.1s ease-in-out;}'
-            frame_styles += '.normal_state{height:40%; width:97%; opacity:1; bottom:0px} .fadeout_state {opacity:0; bottom:-20px;}' 
-            frame_styles += '#resize-frame {-o-transition:opacity 0.5s ease-in-out; z-index:123456789 !important; width: 100%; position: fixed; left: 0; height: 15px; background-image:-o-linear-gradient(bottom, #777, #aaa); cursor:n-resize} #resize-frame:active {background-image:-o-linear-gradient(top, #333, #555); box-shadow:0 0 5px #888}'
-            frame_styles += '#resize-frame.hidden {opacity:0} #resize-frame.display {opacity:1} #resize-frame:before { position:absolute; left:46%; content:""; width:100px; height:100%; background:-o-radial-gradient(25% 50%, 3px 3px, #555 100%, transparent),-o-radial-gradient(36% 50%, 3px 3px, #555 100%, transparent), -o-radial-gradient(47% 50%, 3px 3px, #555 100%, transparent), -o-radial-gradient(25% 54%, 3px 3px, #bbb 100%, transparent),-o-radial-gradient(36% 54%, 3px 3px, #bbb 100%, transparent), -o-radial-gradient(47% 54%, 3px 3px, #bbb 100%, transparent); }'
-            frame_styles += '#close-frame {cursor:pointer; font:bold 16px sans-serif; color: #eee; float:right; top:-5px; margin: 2px 0px; padding: 0 4px; position:relative; border-left:1px solid transparent;} #close-frame:hover {background:rgba(255,255,255,.75); color:#444; border-left: 1px solid #fff}';
+            + '.normal_state{height:40%; width:97%; opacity:1; bottom:0px} .fadeout_state {opacity:0; bottom:-20px;}' 
+            + '#resize-frame {-o-transition:opacity 0.5s ease-in-out; z-index:123456789 !important; width: 100%; position: fixed; left: 0; height: 15px; background-image:-o-linear-gradient(bottom, #777, #aaa); cursor:n-resize} #resize-frame:active {background-image:-o-linear-gradient(top, #333, #555); box-shadow:0 0 5px #888}'
+            + '#resize-frame.hidden {opacity:0} #resize-frame.display {opacity:1} #resize-frame:before { position:absolute; left:46%; content:""; width:100px; height:100%; background:-o-radial-gradient(25% 50%, 3px 3px, #555 100%, transparent),-o-radial-gradient(36% 50%, 3px 3px, #555 100%, transparent), -o-radial-gradient(47% 50%, 3px 3px, #555 100%, transparent), -o-radial-gradient(25% 54%, 3px 3px, #bbb 100%, transparent),-o-radial-gradient(36% 54%, 3px 3px, #bbb 100%, transparent), -o-radial-gradient(47% 54%, 3px 3px, #bbb 100%, transparent); }'
+            + '#close-frame {cursor:pointer; font:bold 16px sans-serif; color: #eee; float:right; top:-5px; margin: 2px 0px; padding: 0 4px; position:relative; border-left:1px solid transparent;} #close-frame:hover {background:rgba(255,255,255,.75); color:#444; border-left: 1px solid #fff}'
+            + '#fix-the-web-comment-frame .report_body {font-size:18px; padding:5px; margin: 2px 1px; background:#fff; border-radius:6px; border:1px solid #c1c1c1;}'
+            + '#fix-the-web-comment-frame .title {color:#777} #fix-the-web-comment-frame .username {color:#000}'
+            + '#fix-the-web-comment-frame .thread {margin:0 0 5px 0; border-radius:6px; padding: 7px; background:#eaeaea;}'
+            + '#fix-the-web-comment-frame a {color:#3399FF; text-decoration:none} #fix-the-web-comment-frame a:hover {text-decoration:underline}'
+            + '#fix-the-web-comment-frame .collapsed .toggle {display:none} #fix-the-web-comment-frame .expanded .toggle {display:block}'
+            + '#fix-the-web-comment-frame .collapsed .change_state {content:\'expand thread\';} #fix-the-web-comment-frame .expanded .change_state {content:\'collapse thread\';}'
+            + '#fix-the-web-comment-frame .change_state {padding: 0 5px; color:#3399FF; font-weight:100} #fix-the-web-comment-frame .change_state:hover {cursor:pointer; text-decoration:underline}'
             
             frame_style_element.setAttribute('type', 'text/css');
             frame_style_element.appendChild(document.createTextNode(frame_styles));
