2aefc3012e7ef0ddf59204c7fcdb90192a41051e
Elias <Elias@172.30.242.145>
2012-03-15 19:41:03 -0800

    made PATCH 2 more concise

--- includes/patches.js
+++ includes/patches.js
@@ -42,30 +42,8 @@
     }
     
     // PATCH-2 (11.62, patch added, goldas.com) fixed submenus on the page
-    else if ( hostname.indexOf('goldas.com') > -1 ) {
-        window.addEventListener('DOMContentLoaded', function() {            
-            var new_function = function P7AniMagic(el,x,y,a,b,c,s) { //v2.9 PVII-Project Seven Development
-                    var xx,yy,i,g,elo=el,f="",m=false,d="",pa='px';if(document.layers){pa='';}
-                    x=parseInt(x);y=parseInt(y);var t='g.p7Magic=setTimeout("P7AniMagic(\''+elo+'\','; 
-                    if((g=MM_findObj(el))!=null){d=(document.layers)?g:g.style;}else{return;}
-                    if(parseInt(s)>0){eval(t+x+','+y+','+a+','+b+','+c+',0)",' + s+')');return;}
-                    xx=parseInt(d.left);if(isNaN(xx)){if(g.currentStyle){xx=parseInt(g.currentStyle.left);
-                    }else if(document.defaultView&&document.defaultView.getComputedStyle){
-                    xx=parseInt(document.defaultView.getComputedStyle(g,"").getPropertyValue("left"));}
-                    if(isNaN(xx)){xx=0;}}yy=parseInt(d.top);if(isNaN(yy)){if(g.currentStyle){yy=parseInt(g.currentStyle.top);
-                    }else if(document.defaultView&&document.defaultView.getComputedStyle){
-                    yy=parseInt(document.defaultView.getComputedStyle(g,"").getPropertyValue("top"));}
-                    if(isNaN(yy)){yy=0;}}if(c==1){x+=xx;y+=yy;m=true;c=0;}else if(c==2){m=false;
-                    if(g.p7Magic){clearTimeout(g.p7Magic);}}else{i=parseInt(a);if(g.p7Magic){clearTimeout(g.p7Magic);}
-                    if(xx<x){xx+=i;m=true;if(xx>x){xx=x;}}if(xx>x){xx-=i;m=true;if(xx<x){xx=x;}}
-                    if(yy<y){yy+=i;m=true;if(yy>y){yy=y;}}if(yy>y){yy-=i;m=true;if(yy<y){yy=y;}}}
-                    if(m){d.left=xx+pa;d.top=yy+pa;eval(t+x+','+y+','+a+','+b+','+c+',0)",'+b+')');} 
-                }, 
-                tag = document.createElement('script');
-                
-            tag.appendChild(document.createTextNode(new_function.toString()));
-            document.body.appendChild(tag);
-        }, false);        
+    else if ( hostname.indexOf('goldas.com') > -1 ) {       
+            window.opera = false;
     }
 })()
 
