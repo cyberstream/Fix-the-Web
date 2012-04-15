23e27364ca9c8596bd22fc2068389df8b0a16934
Martin Kadlec <harou2@gmail.com>
2012-04-15 16:46:41 +0200

    translate.js style fixes and optimizations

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,29 +1,36 @@
-docElements = document.querySelectorAll('[data-i18n], [data-i18n-attr]')
+/*global i18n */
+var docElements = document.querySelectorAll('[data-i18n], [data-i18n-attr]');
 
-if ( typeof i18n != 'undefined' && typeof $ != 'undefined' ) {
-    addEventListener('load', function() {
-        var domText = document.getElementsByTagName('html')[0].innerHTML
-              placeholders = domText.match(/{{(.*?)}}/igm).map(function(v) {
-                                           return v.replace(/({|})/igm, '').toLowerCase();
-                                       });
-        
-        for ( i = 0; i < placeholders.length; i++ ) {
-            var thisPlaceholder = placeholders[i],
-                  thisPhRegex = new RegExp('{{' + thisPlaceholder + '}}', 'igm');
-            
-            if (i18n[thisPlaceholder] != 'undefined') {
-                for ( j = 0; j < docElements.length; j++ ) {
-                    var thisElement = docElements[j],
-                          translateAttr = thisElement.getAttribute('data-i18n-attr')
-                    
-                    thisElement.innerHTML = docElements[j].innerHTML.replace(thisPhRegex, i18n[thisPlaceholder])
-                    
-                    // translate the specified attribute node
-                    if ( translateAttr && thisElement.getAttribute(translateAttr) ) {
-                        thisElement.setAttribute(translateAttr, thisElement.getAttribute(translateAttr).replace(thisPhRegex, i18n[thisPlaceholder]))
-                    }
-                }
-            }
-        }
-    });
+if ( typeof i18n !== 'undefined' && typeof $ !== 'undefined' ) {
+	document.addEventListener('DOMContentLoaded', function() {
+		"use strict";
+		var 
+			domText = document.querySelector('html').innerHTML,
+			placeholders = domText.match(/\{\{(.*?)\}\}/igm).map(function(v) {
+				return v.replace(/(\{|\})/igm, '').toLowerCase();
+			}),
+			i, j, imax, jmax,
+			thisPlaceholder, thisPhRegex,
+			thisElement, translateAttr
+		;
+
+		for ( i = 0, imax = placeholders.length; i < imax; i++ ) {
+			thisPlaceholder = placeholders[i];
+			thisPhRegex = new RegExp('{{' + thisPlaceholder + '}}', 'igm');
+
+			if (i18n[thisPlaceholder] !== 'undefined') {
+				for ( j = 0, jmax = docElements.length; j < jmax; j++ ) {
+					thisElement = docElements[j];
+					translateAttr = thisElement.getAttribute('data-i18n-attr');
+
+					thisElement.innerHTML = docElements[j].innerHTML.replace(thisPhRegex, i18n[thisPlaceholder]);
+
+					// translate the specified attribute node
+					if ( translateAttr && thisElement.getAttribute(translateAttr) ) {
+						thisElement.setAttribute(translateAttr, thisElement.getAttribute(translateAttr).replace(thisPhRegex, i18n[thisPlaceholder]));
+					}
+				}
+			}
+		}
+	});
 }
